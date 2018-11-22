e1f9787a443393bd84ff5d969e026b807138dcbc
Konstantin Shcherban <kostya.scherban@gmail.com>
2018-11-22 13:13:23 +0300

    автоматизация сборки

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 var csso = require("gulp-csso");
 var rename = require("gulp-rename");
+var del = require("del");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -20,15 +21,26 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
+gulp.task("clean", function () {
+  return del("build");
+});
+
 gulp.task("copy", function () {
-  return gulp.src("source/sass/style.min.scss")
-    .pipe(gulp.dest("build/css"))
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/*.html"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"))
     .pipe(server.stream());
 });
 
@@ -46,4 +58,4 @@ gulp.task("server", function () {
 });
 
 gulp.task("start", gulp.series("css", "server"));
-gulp.task("build", gulp.series("css", "copy"));
+gulp.task("build", gulp.series("clean", "css", "copy"));
